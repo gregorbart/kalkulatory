062d35bfa7bfaf8bcc67baa218e5d3d4b642c59d
gregorbart <118559145+gregorbart@users.noreply.github.com>
2022-11-19 20:45:47 +0100

    Create python.py

---
+++ kod/python.py
@@ -0,0 +1,33 @@
+
+num1 = int(input("Wprowadź pierwszą liczbę: "))
+num2 = int(input("Wprowadź drugą liczbę: "))
+
+print("Działania:")
+print("      +: Dodaj")
+print("      -: Odejmnij")
+print("      *: Pomnórz")
+print("      /: Podziel")
+
+dzi = input("Wprowadź działanie: ")
+
+result=0
+
+def printtheresult(n1,n2,div,res):
+    print(f"{n1}{div}{n2}={res}")
+
+if dzi == "+":
+    printtheresult(num1,num2,dzi,result)
+    result=num1+num2
+elif dzi == "-":
+    printtheresult(num1,num2,dzi,result)
+    result=num1-num2
+elif dzi == "*":
+    result=num1*num2
+    printtheresult(num1,num2,dzi,result)
+elif dzi == "/":
+    result=num1/num2
+    printtheresult(num1,num2,dzi,result)
+else:
+    print("Nieprawidłowe działanie")
+
+input("Kliknij enter aby wyłączyć aplikacje...")
